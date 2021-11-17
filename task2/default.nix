63e4630a6bc4108ab17a899694d8dc23b58932e9
Yichao Zhu <yukio.zhu@tum.de>
2021-11-18 03:15:53 +0800

    update close mechanism & add select version

--- task2/default.nix
+++ task2/default.nix
@@ -6,10 +6,13 @@ stdenv.mkDerivation {
 
   buildInputs = [ boost poco ];
 
-  buildPhase = "gcc -o server server.c -lPocoFoundation -lboost_system";
+  buildPhase = ''
+    gcc -o server server.c -lPocoFoundation -lboost_system
+    gcc -o server_select server_select.c -lPocoFoundation -lboost_system
+  '';
 
   installPhase = ''
     mkdir -p $out/bin
-    cp server $out/bin/
+    cp server server_select $out/bin/
   '';
 }
