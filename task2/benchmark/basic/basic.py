31fab3e5d65ac2cd87cb34c7820497d2729d5d67
Georgiy Nefedov <georgiy.nefedov@tum.de>
2021-11-21 20:09:12 +0100

    refactor

--- task2/benchmark/basic/basic.py
+++ task2/benchmark/basic/basic.py
@@ -1,26 +1,37 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+
 import os
+import matplotlib.pyplot as plt
+import pandas as pd
+
 
 BASIC_DATA_FILENAME = 'basic_bench_output.csv'
+BASIC_PLOT_FILENAME = 'basic_plot.png'
+BASIC_PORT = 8081
+
 CONNECTIONS = 'Connections'
 REQS_PER_SEC = 'Requests/Second'
 
-with open(BASIC_DATA_FILENAME, 'w') as f:
-    f.write(f'{CONNECTIONS},{REQS_PER_SEC}\n')
-    f.flush()
-    for i in range(10, 101, 10):
-        print(f'Running wrk with {i} connections')
-        f.write(f'{i},')
+
+def benchmark(data_filename, plot_filename, port):
+    with open(data_filename, 'w') as f:
+        f.write(f'{CONNECTIONS},{REQS_PER_SEC}\n')
         f.flush()
-        out = os.popen(
-            f'wrk -t 10 -c {i} http://localhost:8081 | grep Requests/sec')
-        line = out.read()
-        recsPerSec = line.split(' ')[-1]
-        print('requests per second', recsPerSec)
-        f.write(recsPerSec)
-        f.flush()
-df = pd.read_csv(BASIC_DATA_FILENAME)
-print(df)
-df.plot(x=CONNECTIONS, y=REQS_PER_SEC, xlim=[0, 110])
-plt.savefig('basic_plot.png')
+        for i in range(10, 101, 10):
+            print(f'Running wrk with {i} connections')
+            f.write(f'{i},')
+            f.flush()
+            out = os.popen(
+                f'wrk -t 10 -c {i} http://localhost:{port} | grep Requests/sec')
+            line = out.read()
+            recsPerSec = line.split(' ')[-1]
+            print('requests per second', recsPerSec)
+            f.write(recsPerSec)
+            f.flush()
+
+    df = pd.read_csv(data_filename)
+    print(df)
+    df.plot(x=CONNECTIONS, y=REQS_PER_SEC, ylim=[0, 110000])
+    plt.savefig(plot_filename)
+
+
+benchmark(BASIC_DATA_FILENAME, BASIC_PLOT_FILENAME, BASIC_PORT)
