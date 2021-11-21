e0e14b5b77df855633930cea27ab61b18ed72a2c
Georgiy Nefedov <georgiy.nefedov@tum.de>
2021-11-21 18:28:43 +0100

    add matplotlib import

--- task2/default.nix
+++ task2/default.nix
@@ -1,10 +1,15 @@
 with import <nixpkgs> {};
+{ pkgs ? import (fetchTarball https://git.io/Jf0cc) {} }:
 
+let customPython = pkgs.python38.buildEnv.override {
+  extraLibs = [ pkgs.python38Packages.pandas pkgs.python38Packages.numpy pkgs.python38Packages.matplotlib ];
+};
+in
 stdenv.mkDerivation {
   name = "server";
   src = ./.;
 
-  buildInputs = [ boost poco ];
+  buildInputs = [ boost poco customPython];
 
   buildPhase = ''
     gcc -o server server.c -lPocoFoundation -lboost_system
@@ -16,8 +21,5 @@ stdenv.mkDerivation {
   installPhase = ''
     mkdir -p $out/bin
     cp server server_select server_epoll server_thread $out/bin/
-  '';
-  shellHook = ''
-    nix-shell -p python38Packages.pandas python35Packages.numpy
   '';
 }
