9c7367afb634ba504c4abe671674aab2d0876712
dbachar <dan.bachar@tum.de>
2021-11-23 00:48:46 +0100

    implement io_uring

---
+++ task2/server_uring.c
@@ -0,0 +1,208 @@
+#include <stdio.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <liburing.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <net/if.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define QUEUE_DEPTH             256
+#define READ_SZ                 2048
+
+#define EVENT_TYPE_ACCEPT       0
+#define EVENT_TYPE_READ         1
+#define EVENT_TYPE_WRITE        2
+
+char *OK = "HTTP/1.1 200 OK\r\nContent-Length: 22\r\nContent-Type: text/html\r\n\r\nHello World from ATeam";
+
+struct request {
+    int event_type;
+    int iovec_count;
+    int client_socket;
+    struct iovec iov[];
+};
+
+struct io_uring ring;
+
+/*
+ One function that prints the system call and the error details
+ and then exits with error code 1. Non-zero meaning things didn't go well.
+ */
+void fatal_error(const char *syscall) {
+    perror(syscall);
+    exit(EXIT_FAILURE);
+}
+
+/*
+ * Helper function for cleaner looking code.
+ * */
+
+void *zh_malloc(size_t size) {
+    void *buf = malloc(size);
+    if (!buf) {
+        fprintf(stderr, "Fatal error: unable to allocate memory.\n");
+        exit(EXIT_FAILURE);
+    }
+    return buf;
+}
+
+/*
+ * This function is responsible for setting up the main listening socket used by the
+ * web server.
+ * */
+
+int setup_listening_socket(int port) {
+    struct sockaddr_in6 address;
+    int server_socket, sock, addrlen = sizeof(address), reuse = 1;
+    uint32_t interfaceIndex;
+
+    if ((server_socket = socket(AF_INET6, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR,
+                   &reuse, sizeof(reuse)))
+    {
+        perror("setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
+
+    interfaceIndex = if_nametoindex("swissknife0");
+    address.sin6_scope_id = interfaceIndex;
+    address.sin6_family = AF_INET6;
+    address.sin6_addr = in6addr_any;
+    address.sin6_port = htons(port);
+    if (bind(server_socket, (struct sockaddr *)&address,
+             addrlen) < 0)
+    {
+        fatal_error("bind()");
+    }
+    if (listen(server_socket, 3) < 0)
+    {
+        fatal_error("listen()");
+    }
+
+    return (server_socket);
+}
+
+int add_accept_request(int server_socket, struct sockaddr_in *client_addr,
+                       socklen_t *client_addr_len) {
+    struct io_uring_sqe *sqe = io_uring_get_sqe(&ring);
+    io_uring_prep_accept(sqe, server_socket, (struct sockaddr *) client_addr,
+                         client_addr_len, 0);
+    struct request *req = malloc(sizeof(*req));
+    req->event_type = EVENT_TYPE_ACCEPT;
+    io_uring_sqe_set_data(sqe, req);
+    io_uring_submit(&ring);
+
+    return 0;
+}
+
+int add_read_request(int client_socket) {
+    struct io_uring_sqe *sqe = io_uring_get_sqe(&ring);
+    struct request *req = malloc(sizeof(*req) + sizeof(struct iovec));
+    req->iov[0].iov_base = malloc(READ_SZ);
+    req->iov[0].iov_len = READ_SZ;
+    req->event_type = EVENT_TYPE_READ;
+    req->client_socket = client_socket;
+    memset(req->iov[0].iov_base, 0, READ_SZ);
+    /* Linux kernel 5.5 has support for readv, but not for recv() or read() */
+    io_uring_prep_readv(sqe, client_socket, &req->iov[0], 1, 0);
+    io_uring_sqe_set_data(sqe, req);
+    io_uring_submit(&ring);
+    return 0;
+}
+
+int add_write_request(struct request *req) {
+    struct io_uring_sqe *sqe = io_uring_get_sqe(&ring);
+    req->event_type = EVENT_TYPE_WRITE;
+    io_uring_prep_writev(sqe, req->client_socket, req->iov, req->iovec_count, 0);
+    io_uring_sqe_set_data(sqe, req);
+    io_uring_submit(&ring);
+    return 0;
+}
+
+void _send_static_string_content(const char *str, int client_socket) {
+    struct request *req = zh_malloc(sizeof(*req) + sizeof(struct iovec));
+    unsigned long slen = strlen(str);
+    req->iovec_count = 1;
+    req->client_socket = client_socket;
+    req->iov[0].iov_base = zh_malloc(slen);
+    req->iov[0].iov_len = slen;
+    memcpy(req->iov[0].iov_base, str, slen);
+    add_write_request(req);
+}
+
+void handle_client_request(struct request *req) {
+    _send_static_string_content(OK, req->client_socket);
+}
+
+void server_loop(int server_socket) {
+    struct io_uring_cqe *cqe;
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+
+    add_accept_request(server_socket, &client_addr, &client_addr_len);
+
+    while (1) {
+        int ret = io_uring_wait_cqe(&ring, &cqe);
+        struct request *req = (struct request *) cqe->user_data;
+        if (ret < 0)
+            fatal_error("io_uring_wait_cqe");
+        if (cqe->res < 0) {
+            fprintf(stderr, "Async request failed: %s for event: %d\n",
+                    strerror(-cqe->res), req->event_type);
+            exit(EXIT_FAILURE);
+        }
+
+        switch (req->event_type) {
+            case EVENT_TYPE_ACCEPT:
+                add_accept_request(server_socket, &client_addr, &client_addr_len);
+                add_read_request(cqe->res);
+                free(req);
+                break;
+            case EVENT_TYPE_READ:
+                if (!cqe->res) {
+                    fprintf(stderr, "Empty request!\n");
+                    break;
+                }
+                handle_client_request(req);
+                free(req->iov[0].iov_base);
+                free(req);
+                break;
+            case EVENT_TYPE_WRITE:
+                for (int i = 0; i < req->iovec_count; i++) {
+                    free(req->iov[i].iov_base);
+                }
+                close(req->client_socket);
+                free(req);
+                break;
+        }
+        /* Mark this request as processed */
+        io_uring_cqe_seen(&ring, cqe);
+    }
+}
+
+void sigint_handler(int signo) {
+    printf("^C pressed. Shutting down.\n");
+    io_uring_queue_exit(&ring);
+    exit(EXIT_SUCCESS);
+}
+
+int main() {
+    int port = 64005;
+    int server_socket = setup_listening_socket(port);
+
+    signal(SIGINT, sigint_handler);
+    io_uring_queue_init(QUEUE_DEPTH, &ring, 0);
+    server_loop(server_socket);
+
+    return EXIT_SUCCESS;
+}
