892fb05b6b62ca1ff3b852b1bd4c8bc89e6badcf
dbachar <dan.bachar@tum.de>
2021-11-23 00:48:46 +0100

    implement io_uring

--- task2/default.nix
+++ task2/default.nix
@@ -9,7 +9,7 @@ stdenv.mkDerivation {
   name = "server";
   src = ./.;
 
-  buildInputs = [ boost poco customPython];
+  buildInputs = [ boost poco customPython liburing ];
 
   buildPhase = ''
     gcc -o server server.c -lPocoFoundation -lboost_system
@@ -17,10 +17,11 @@ stdenv.mkDerivation {
     gcc -o server_epoll server_epoll.c -lPocoFoundation -lboost_system
     gcc -o server_thread server_thread.c -lPocoFoundation -lboost_system -lpthread
     gcc -o server_epoll_multitask server_epoll_multitask.c -lPocoFoundation -lboost_system
+    gcc -o server_uring -g server_uring.c -lPocoFoundation -lboost_system -l:liburing.a
   '';
 
   installPhase = ''
     mkdir -p $out/bin
-    cp server server_select server_epoll server_thread server_epoll_multitask $out/bin/
+    cp server server_select server_epoll server_thread server_epoll_multitask server_uring $out/bin/
   '';
 }
