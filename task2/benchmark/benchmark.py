a09b00542583af6720cdedcd05304afa8c8d7028
Georgiy Nefedov <georgiy.nefedov@tum.de>
2021-11-24 18:59:47 +0100

    add dynamic ip resolving for swissknife0 interface

--- task2/benchmark/benchmark.py
+++ task2/benchmark/benchmark.py
@@ -1,12 +1,23 @@
 import os
 import matplotlib.pyplot as plt
 import pandas as pd
+import netifaces as ni
 
 CONNECTIONS = 'Connections'
 REQS_PER_SEC = 'Requests/Second'
 DATA_FILENAME = 'benchmark_output.csv'
 PLOT_FILENAME = 'benchmark_plot.png'
 PORT = 8081
+
+# retrieve the ip address of the swissknife0 interface
+ip = list(
+    filter(lambda ip: "swissknife0" in ip,
+           (map(lambda ip: ip["addr"],
+                ni.ifaddresses('swissknife0')[ni.AF_INET6])
+            )
+           )
+).pop()
+URL = f"http://[{ip}]"
 
 
 def benchmark(data_filename, plot_filename, port):
@@ -18,7 +29,7 @@ def benchmark(data_filename, plot_filename, port):
             f.write(f'{i},')
             f.flush()
             out = os.popen(
-                f'wrk -t 10 -c {i} http://localhost:{port} | grep Requests/sec')
+                f'wrk -t 10 -c {i} {URL}:{port} | grep Requests/sec')
             line = out.read()
             recsPerSec = line.split(' ')[-1]
             print('requests per second', recsPerSec)
@@ -29,5 +40,6 @@ def benchmark(data_filename, plot_filename, port):
     print(df)
     df.plot(x=CONNECTIONS, y=REQS_PER_SEC)
     plt.savefig(plot_filename)
+
 
 benchmark(DATA_FILENAME, PLOT_FILENAME, PORT)
