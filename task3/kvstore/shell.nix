e11ffaff66fb7291329d56d18dfc95a4159b4db1
Georgiy <georgiy.nefedov@tum.de>
2021-12-16 09:36:36 +0000

    :q remove large file

---
+++ task3/kvstore/shell.nix
@@ -0,0 +1,13 @@
+# shell.nix
+let
+  pkgs = import <nixpkgs>  {};
+  stdenv = pkgs.stdenv;
+  python3 = pkgs.python3.withPackages(p: with p; [
+    # python packages here
+    psutil
+  ]);
+in
+  stdenv.mkDerivation {
+    name = "env";
+    buildInputs = with pkgs; [ hello gnumake pkgs.gcc48 pkgs.gflags pkgs.zlib pkgs.bzip2 pkgs.lz4 pkgs.zstd pkgs.jdk11_headless pkgs.libevent pkgs.memcached pkgs.maven python3];
+}  
