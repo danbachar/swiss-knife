e1aebf64d01cca6d1a515e947f5d20a3057766d8
Georgiy <georgiy.nefedov@tum.de>
2021-12-17 16:41:43 +0000

    implement reproducible experiment

--- task3/kvstore/reproduce.py
+++ task3/kvstore/reproduce.py
@@ -1,99 +1,11 @@
 #!/usr/bin/env python3
 import os
-import random
-import string
 import shutil
-from contextlib import closing
-import socket
-import signal
-import psutil
+from helpers.constants import RESULT_FOLDER
 
-RND = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
-ROCKSDB_PATH = f'/tmp/teama_rocksdb_benchmark_db_{RND}'
+if os.path.exists(RESULT_FOLDER):
+    shutil.rmtree(RESULT_FOLDER)
+os.makedirs(f'{RESULT_FOLDER}/rocksdb')
+os.makedirs(f'{RESULT_FOLDER}/memcached')
 
-def log(msg):
-    print("\n")
-    print("#" * (12 + len(msg)))
-    print("#"*5, msg, "#"*5)
-    print("#" * (12 + len(msg)))
-    print("\n")
-
-def cloneRocksDB():
-    """Clone rocksdb from git"""
-    if not os.path.exists("rocksdb"):
-        #shutil.rmtree("rocksdb")
-        os.system("git clone https://github.com/facebook/rocksdb.git")
-
-def downloadYCSB():
-    """Download YCSB from git"""
-    if os.path.exists("YCSB"):
-        shutil.rmtree("YCSB")
-    os.system("curl -O --location https://github.com/brianfrankcooper/YCSB/releases/download/0.17.0/ycsb-0.17.0.tar.gz && tar xfvz ycsb-0.17.0.tar.gz && mv ycsb-0.17.0 YCSB")
-    
-def cloneYCSB():
-    """Clone YCSB from git"""
-    if os.path.exists("YCSB"):
-        shutil.rmtree("YCSB")
-    os.system("git clone http://github.com/brianfrankcooper/YCSB.git")
-    
-    
-# Taken from here https://stackoverflow.com/a/45690594
-def find_free_port():
-    """Find and return a port that is not taken"""
-    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
-        s.bind(('', 0))
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        return s.getsockname()[1]
-    
-def shutdown_memcached():
-    """Kill 'memcached' processes started by user 'teama'"""
-    for proc in psutil.process_iter():
-        try:
-            # Get process name & pid from process object.
-            processName = proc.name()
-            processID = proc.pid
-            processUsername = proc.username()
-            if "memcached" in processName and "teama" in processUsername:
-                print('killing ', processName , ' PID: ', processID, ' USERNAME: ', processUsername)
-                os.kill(processID, signal.SIGINT)
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
-    
-log("CLONE AND BUILD ROCKSDB. MAY TAKE A WHILE. PLEASE BE PATIENT.")
-cloneRocksDB()
-# build static_lib
-os.system("cd rocksdb && make static_lib && cd ../")
-# build shared_lib
-os.system("cd rocksdb && make shared_lib && cd ../ ")
-log("DOWNLOAD YCSB FOR ROCKSDB")
-downloadYCSB()
-
-log("CREATE A ROCKSDB INSTANCE")
-# build initRocksDB
-os.system("make")
-# init a rocksdb instance for testing
-os.system(f'./initRocksDB {ROCKSDB_PATH}')
-
-log("RUN BENCHMARKS ON ROCKSDB")
-# run benchmark on rocksdb instance
-os.system(f'cd YCSB/ && ./bin/ycsb.sh load rocksdb -s -P workloads/workloada -p rocksdb.dir="{ROCKSDB_PATH}" -p recordcount=500000 -threads 16 && cd ../')
-
-log("START MEMCACHED SERVER")
-PORT = find_free_port()
-
-# start memcached server
-os.popen(f"memcached -d -p {PORT}")
-
-log("CLONE YCSB FOR MEMCACHED")
-cloneYCSB()
-
-log("BUILD YCSB FOR MEMCACHED")
-os.system("cd YCSB && mvn -pl site.ycsb:memcached-binding -am clean package && cd ../")
-
-log("RUN BENCHMARKS ON MEMCACHED")
-
-# TODO
-
-log("KILL MEMCACHED PROCESSES")
-# shutdown memcached server started by teama
-shutdown_memcached()
+from helpers import reproduce_rocksdb, reproduce_memcached, plot
--- task3/kvstore/shell.nix
+++ task3/kvstore/shell.nix
@@ -5,9 +5,11 @@ let
   python3 = pkgs.python3.withPackages(p: with p; [
     # python packages here
     psutil
+    pandas
+    matplotlib
   ]);
 in
   stdenv.mkDerivation {
     name = "env";
-    buildInputs = with pkgs; [ hello gnumake pkgs.gcc48 pkgs.gflags pkgs.zlib pkgs.bzip2 pkgs.lz4 pkgs.zstd pkgs.jdk11_headless pkgs.libevent pkgs.memcached pkgs.maven python3];
+    buildInputs = with pkgs; [ hello gnumake pkgs.gcc48 pkgs.gflags pkgs.zlib pkgs.bzip2 pkgs.lz4 pkgs.zstd pkgs.jdk11_headless pkgs.libevent pkgs.memcached pkgs.libmemcached pkgs.maven python3 pkgs.telnet];
 }  
