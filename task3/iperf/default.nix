6115b59974e79edeb5c65eb6db53390592d21235
dbachar <dan.bachar@tum.de>
2021-12-29 02:23:22 +0100

    "modify build file"

--- task3/iperf/default.nix
+++ task3/iperf/default.nix
@@ -1,16 +1,16 @@
-# shell.nix
-let
-  pkgs = import <nixpkgs>  {};
-  stdenv = pkgs.stdenv;
-  python3 = pkgs.python3.withPackages(p: with p; [
-    # python packages here
-    psutil
-    pandas
-    netifaces
-    matplotlib
-  ]);
-in
-  stdenv.mkDerivation {
-    name = "env";
-    buildInputs = with pkgs; [ hello gnumake pkgs.gcc48 pkgs.gflags pkgs.zlib pkgs.bzip2 pkgs.lz4 pkgs.zstd pkgs.jdk11_headless pkgs.libevent pkgs.memcached pkgs.libmemcached pkgs.maven python3 pkgs.telnet];
-}  
+    # shell.nix
+    let
+    pkgs = import <nixpkgs>  {};
+    stdenv = pkgs.stdenv;
+    python3 = pkgs.python3.withPackages(p: with p; [
+        # python packages here
+        psutil
+        netifaces
+        pandas
+        matplotlib
+    ]);
+    in
+    stdenv.mkDerivation {
+        name = "env";
+        buildInputs = with pkgs; [ python3 pkgs.telnet];
+    }  
