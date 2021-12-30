4c25d2087a9c9c9ea385d4fb011344852483d845
dbachar <dan.bachar@tum.de>
2021-12-30 11:15:03 +0100

    ignore output of open_port

--- task3/iperf/server.py
+++ task3/iperf/server.py
@@ -37,18 +37,6 @@ def open_port() -> None:
                                 stderr=subprocess.PIPE,
                                 universal_newlines=True)
 
-    while True:
-        output = process.stdout.readline()
-        print(output.strip())
-        # Do something else
-        return_code = process.poll()
-        if return_code is not None:
-            print('RETURN CODE', return_code)
-            # Process has finished, read rest of the output
-            for output in process.stdout.readlines():
-                print(output.strip())
-            break
-
 def main() -> None:
     open_port()
     addrs = map(lambda ip: ip["addr"], ni.ifaddresses('swissknife0')[ni.AF_INET6])
