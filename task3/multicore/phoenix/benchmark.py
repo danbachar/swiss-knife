ce919611cde0e847565358caf51dbf16cab04f57
Yichao Zhu <yukio.zhu@tum.de>
2022-01-09 11:34:36 +0800

    use official dataset for phoenix++

--- task3/multicore/phoenix/benchmark.py
+++ task3/multicore/phoenix/benchmark.py
@@ -8,23 +8,22 @@ import multiprocessing
 import matplotlib.pyplot as plt
 
 dataDir     = "/root/dataset/"
-inputSizes    = ["small","medium","large"]
-textNames   = ["dickens-20M.txt", "dickens-100M.txt", "dickens-200M.txt"]  # as 500M is too heavy for the sequential version
-textSets    = [ dataDir + text  for text in textNames]
-keysToMatch = dataDir + "keys.txt"
+inputSizes  = ["small","med","large"]
+keyfiles    = ["key_file_50MB.txt","key_file_100MB.txt","key_file_500MB.txt"]  # as 500M is too heavy for the sequential version
+textNames   = ["word_10MB.txt","word_50MB.txt","word_100MB.txt"]
 
 params = {
-    "histogram": [dataDir + bmpSize + ".bmp" for bmpSize in inputSizes],
+    "histogram": [dataDir + "histogram_datafiles/" + bmpSize + ".bmp" for bmpSize in inputSizes],
     "kmeans": ["-d 10 -c 50 -p 10000 -s 9999","-d 10 -c 50 -p 50000 -s 9999","-d 10 -c 50 -p 100000 -s 9999"],
-    "linear_regression": textSets,
-    "matrix_multiply": ["100 100 matrix_file","500 500 matrix_file","1000 1000 matrix_file"],
+    "linear_regression": [ dataDir + "linear_regression_datafiles/" + text for text in keyfiles],
+    "matrix_multiply": ["100 100 matrix_file","800 800 matrix_file","1000 1000 matrix_file"],
     "pca": ["-r 300 -c 300 -s 9999","-r 500 -c 500 -s 9999","-r 1000 -c 1000 -s 9999"],
-    "string_match": [keysToMatch + " " + textSet for textSet in textSets], 
-    "word_count": textSets
+    "string_match": [ dataDir + "string_match_datafiles/" + text for text in keyfiles],
+    "word_count": [ dataDir + "word_count_datafiles/" + text for text in textNames]
 }
 
-programs = ["histogram", "kmeans", "pca","word_count"]
-baseApps = ["hist-seq", "kmeans-seq", "pca-seq", "wordcount_serial"]
+programs = ["histogram", "kmeans", "linear_regression", "matrix_multiply", "pca", "string_match", "word_count"]
+baseApps = ["hist-seq", "kmeans-seq","lreg-seq", "matrix_mult_serial", "pca-seq", "string_match_serial", "wordcount_serial"]
 
 def benchmark(programs, threads, dataset, base):
     if base == True:
@@ -99,20 +98,40 @@ def plot(programs, mode):
     width = total_width / n
 
     plt.figure(figsize=(12, 6.5))
+    plt.ylim(0, 80 if mode == "threads" else 50)
 
     for i in range(n):
         plt.bar(x, num_lists[i], width=width, label=(legends[i]+" Threads" if mode == "threads" else inputSizes[legends[i]]))
+        for j in range(len(x)):
+            plt.text(x[j], 0, str(round(num_lists[i][j],1)), ha='center', va='bottom', fontsize=5, color='black')
         for i in range(len(x)):
             x[i] = x[i] + width
 
     x = [i + width for i in list(range(len(num_lists[0])))]
     plt.xticks(x, labels=programs)
+    plt.xlabel("Programs")
+    plt.ylabel("Speed Up")
 
     plt.legend()
     plt.savefig('/root/result/result_t.png' if mode == "threads" else f'/root/result/result_d.png')
 
 def generate_data():
-    os.system('python3 ~/dataset/prepare_dataset.py')
+    #os.system('python3 ~/dataset/prepare_dataset.py')
+    #csl.stanford.edu server is running now, so use the official dataset instead
+    import wget
+    import tarfile
+    print("Downloading datasets!")
+    prefix="http://csl.stanford.edu/~christos/data/"
+    suffix=".tar.gz"
+    files=["histogram","linear_regression","string_match","reverse_index","word_count"]
+    for file in files:
+        print(file+suffix)
+        fname=prefix+file+suffix
+        wget.download(fname, out=dataDir)
+        print("untaring")
+        tar = tarfile.open(dataDir+file+suffix, "r:gz")
+        tar.extractall(dataDir)
+        tar.close()
 
 def main():
     generate_data()
@@ -120,7 +139,7 @@ def main():
     for threads in [1,2,4,8]:
         os.environ["MR_NUMTHREADS"] = str(threads)
         print(f'Threads Num: {threads}')
-        benchmark(programs, threads, 2, False)  #fix dataset to the large one
+        benchmark(programs, threads, 1, False)  #fix dataset to the large one
 
     for dataset in range(3):
         benchmark(programs, 8, dataset, False)  #fix threads to 8
