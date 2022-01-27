85c12b770ea8c631703e469648b9496f8fb5db27
dbachar <dan.bachar@tum.de>
2022-01-27 16:42:28 +0100

    "fix copy bug"

--- task4/Assignment1/default.nix
+++ task4/Assignment1/default.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/bc59ba1>
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/bc59ba15b64d0a0ee1d1764f18b4f3480d2c3e5a.tar.gz") {}
 }:
 pkgs.mkShell {
   nativeBuildInputs = [
